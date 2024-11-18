cf32790db127f3a9526085cebfea02a8c9c02773
shubhampandey45 <shubhampandey5410@gmail.com>
2024-11-18 23:15:56 +0530

    Resolved Conflicts

--- app/src/main/java/com/sp45/android_animations/MainScreen.kt
+++ app/src/main/java/com/sp45/android_animations/MainScreen.kt
@@ -69,8 +69,8 @@ fun MainScreen(navController: NavController) {
         AnimationItem("Expanding Rings", Color(0xFF4CAF50)),
         AnimationItem("Orbiting Objects", Color(0xFF3F51B5)),
         AnimationItem("Card Flipping", Color(0xFFE91E63)),
-        AnimationItem("Button to Image", Color(0xFFFF9800)),
-        AnimationItem("Image Transition", Color(0xFF6200EE)),
+        AnimationItem("Button to Image",Color(0xFFFF9800)),
+        AnimationItem("Image Transition",Color(0xFF6200EE)),
         AnimationItem("Sliding Door", Color(0xFF00BCD4)),
         AnimationItem("Text Explosion", Color(0xFFE91E63))
         )
